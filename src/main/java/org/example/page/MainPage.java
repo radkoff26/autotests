67992aa25db2cf5180f1204c7fe75b15b49f5d08
radkoff26 <radko.slava@mail.ru>
2023-03-23 21:32:33 +0300

    classwork

--- src/main/java/org/example/page/MainPage.java
+++ src/main/java/org/example/page/MainPage.java
@@ -9,6 +9,8 @@ import static com.codeborne.selenide.Selenide.$;
 public class MainPage extends Page {
     private static final String URL = "https://ok.ru/feed";
     private final SelenideElement profileName = $(By.className("toolbar_accounts-user_name"));
+    private final SelenideElement tools = $(By.className("js-toolbar-menu"));
+    private final SelenideElement language = $(By.xpath("//div[@class='toolbar_accounts-menu']/ul/li[position() = 2]/a"));
 
     public MainPage() {
         super(URL);
@@ -16,5 +18,17 @@ public class MainPage extends Page {
 
     public String getProfileName() {
         return profileName.getOwnText();
+    }
+
+    public void changeLanguageToEnglish() {
+        tools.click();
+        language.click();
+        final SelenideElement english = $(By.xpath("//a[text()='English']"));
+        english.click();
+    }
+
+    public String getLanguage() {
+        final SelenideElement lang = $(By.xpath("//div[@class='toolbar_accounts-menu']/ul/li[position() = 2]/a/div/span"));
+        return lang.getOwnText();
     }
 }
--- src/test/java/org/example/MainTest.java
+++ src/test/java/org/example/MainTest.java
@@ -3,8 +3,12 @@ package org.example;
 import org.example.model.UserCredentials;
 import org.example.page.LoginPage;
 import org.example.page.MainPage;
-import org.junit.jupiter.api.Assertions;
+import org.hamcrest.BaseMatcher;
+import org.hamcrest.Description;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
 
 class MainTest {
 
@@ -12,9 +16,45 @@ class MainTest {
     void test() {
         LoginPage loginPage = new LoginPage();
         loginPage.openPage();
-        loginPage.login(new UserCredentials("YOUR_PHONE_NUMBER_OR_EMAIL", "YOUR_PASSWORD"));
+        loginPage.login(new UserCredentials("botS23AT20", "autotests2023"));
 
         MainPage mainPage = new MainPage();
-        Assertions.assertEquals("Вячеслав Радько", mainPage.getProfileName());
+
+        assertThat(mainPage.getProfileName(), new BaseMatcher<>() {
+            @Override
+            public boolean matches(Object o) {
+                return o.equals("botS23AT20 botS23AT20");
+            }
+
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("botS23AT20 botS23AT20");
+            }
+        });
+    }
+
+    @Test
+    void languageChangeTest() {
+        LoginPage loginPage = new LoginPage();
+        loginPage.openPage();
+        loginPage.login(new UserCredentials("botS23AT20", "autotests2023"));
+
+        MainPage mainPage = new MainPage();
+
+        mainPage.openPage();
+
+        mainPage.changeLanguageToEnglish();
+
+        assertThat(mainPage.getLanguage(), new BaseMatcher<>() {
+            @Override
+            public boolean matches(Object o) {
+                return o.equals("English");
+            }
+
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("English");
+            }
+        });
     }
 }
